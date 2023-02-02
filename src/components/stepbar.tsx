24b9743e205ce980ac477a7cc8d5e35a2f351c66
sayyyyyy <d958956a6b650@gmail.com>
2023-02-02 18:44:54 +0900

    fix: #22 stepbar.tsxの修正

--- src/components/stepbar.tsx
+++ src/components/stepbar.tsx
@@ -1,34 +1,25 @@
-import { Stepper, Button, Group } from '@mantine/core';
-import {useState} from 'react'
-import { useDispatch, useSelector } from "react-redux";
-// import { counterSlice, CounterState, store,selectCount } from "../pages/_app";
-import {counterSlice, CounterState, selectCount} from '../redux/counterSlice'
+// ライブラリインポート
+import { Stepper } from '@mantine/core';
+
+// 状態管理
+import { useSelector } from "react-redux";
+import { selectCount} from '../redux/counterSlice'
+
 const Stepbar = () => {
-        // const [active, setActive] = useState(1);
-        // const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
-        // const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
-        const dispatch = useDispatch();
-        const selector = useSelector(selectCount);
-        const { increment,decrement } = counterSlice.actions;
+	// 状態管理
+	const selector = useSelector(selectCount);
 
-        return (
-            <>
-              <div className="mt-5">
-                <Stepper color="yellow" active={selector} onStepClick={selector} breakpoint="sm">
-                    <Stepper.Step label="課題">
-                    </Stepper.Step>
-                    <Stepper.Step label="解決策の提案" >
-                    </Stepper.Step>
-                    <Stepper.Step label="ドキュメント化" >
-
-                    </Stepper.Step>
-                    {/* <Stepper.Completed>
-                      Completed, click back button to get to previous step
-                    </Stepper.Completed> */}
-                </Stepper>
-              </div>
-              </>
-            )
+	return (
+		<>
+			<div className="mt-5">
+				<Stepper color="yellow" active={selector} onStepClick={selector} breakpoint="sm">
+					<Stepper.Step label="課題"></Stepper.Step>
+					<Stepper.Step label="解決策の提案" ></Stepper.Step>
+					<Stepper.Step label="ドキュメント化" ></Stepper.Step>
+				</Stepper>
+			</div>
+		</>
+	)
 }       
 
 export default Stepbar
